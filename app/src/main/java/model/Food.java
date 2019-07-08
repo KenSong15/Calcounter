1eafcca4434d224e808c3b757f76321968fb3945
Ken <kensong96@gmail.com>
2019-07-08 11:26:31 -0400

    143D

--- app/src/main/java/model/Food.java
+++ app/src/main/java/model/Food.java
@@ -11,4 +11,49 @@ public class Food implements Serializable {
     private int foodId;
     private String recordDate;
 
+    public Food(String foodName, int calories, int foodId, String recordDate) {
+        this.foodName = foodName;
+        this.calories = calories;
+        this.foodId = foodId;
+        this.recordDate = recordDate;
+    }
+
+    public Food() {//empty constructor
+    }
+
+    public static long getSerialCersionUID() {
+        return serialCersionUID;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(int foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getRecordDate() {
+        return recordDate;
+    }
+
+    public void setRecordDate(String recordDate) {
+        this.recordDate = recordDate;
+    }
 }
