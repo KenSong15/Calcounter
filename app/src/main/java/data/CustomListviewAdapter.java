c6418bf0de942099399f2c2d55b85c05dfc5ee59
Ken <kensong96@gmail.com>
2019-07-08 15:25:01 -0400

    144D

--- app/src/main/java/data/CustomListviewAdapter.java
+++ app/src/main/java/data/CustomListviewAdapter.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.kens.calcounter.R;
 
 import java.util.ArrayList;
 
@@ -52,6 +56,37 @@ public class CustomListviewAdapter extends ArrayAdapter<Food> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View row = convertView;
+        ViewHolder holder = null;
+
+        if(row == null || (row.getTag() == null)) {
+            LayoutInflater inflater = LayoutInflater.from(activity);
+            row = inflater.inflate(layoutResource, null);
+
+            holder = new ViewHolder();
+
+            holder.foodName = (TextView) row.findViewById(R.id.name);
+            holder.foodDate = (TextView) row.findViewById(R.id.dataText);
+            holder.foodCalories = (TextView) row.findViewById(R.id.calories);
+
+            row.setTag(holder);
+        } else {
+            holder = (ViewHolder) row.getTag();
+        }
+
+        holder.food = getItem(position);
+
+        holder.foodName.setText(holder.food.getFoodName());
+        holder.foodDate.setText(holder.food.getRecordDate());
+        holder.foodCalories.setText(String.valueOf(holder.food.getCalories()));
+
+        row.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("click on row", "shoule go to the detail activity");
+            }
+        });
 
 
 
