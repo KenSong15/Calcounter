06af618cc07371b3851c7c411dcc1e2a10150adc
Ken <kensong96@gmail.com>
2019-07-07 21:54:49 -0400

    at V143 11 min,making food model

---
+++ app/src/main/java/model/Food.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.io.Serializable;
+
+public class Food implements Serializable {
+
+    private static final long
+
+    private String foodName;
+    private int calories;
+    private int foodId;
+    private String recordDate;
+
+}
