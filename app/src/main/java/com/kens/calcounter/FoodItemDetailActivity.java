62be0a752c968c0aeddabc5a1337520fc963e0f2
Ken <kensong96@gmail.com>
2019-07-08 21:34:50 -0400

    146D, without delete dropdown menu

--- app/src/main/java/com/kens/calcounter/FoodItemDetailActivity.java
+++ app/src/main/java/com/kens/calcounter/FoodItemDetailActivity.java
@@ -1,10 +1,14 @@
 package com.kens.calcounter;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import model.Food;
 
@@ -35,6 +39,43 @@ public class FoodItemDetailActivity extends AppCompatActivity {
         calories.setTextSize(34.9f);
         calories.setTextColor(Color.RED);
 
+        shareButtion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareCals();
+            }
+        });
+
 
     }
+
+    public void shareCals() {
+
+        StringBuilder dataString = new StringBuilder();
+
+        String name = foodName.getText().toString();
+        String cals = calories.getText().toString();
+        String date = dateTaken.getText().toString();
+
+        dataString.append(" Food: " + name + "\n");
+        dataString.append(" Calories: " + cals + "\n");
+        dataString.append(" Eaten on: " + date);
+
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+
+        i.putExtra(Intent.EXTRA_SUBJECT, "My Caloric Intake");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"kensong96@gmail.com"});
+        i.putExtra(Intent.EXTRA_TEXT, dataString.toString());
+
+        try{
+            startActivity(Intent.createChooser(i,"Send email..."));
+        } catch(ActivityNotFoundException e){
+            Toast.makeText(getApplicationContext(), "Please install any email application to send email.",Toast.LENGTH_LONG).show();
+        }
+
+
+
+    }
+
 }
